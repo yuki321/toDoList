30e8c6a448e30c687dec34d682ab98ff637fc9ee
yuki321 <yishii0321.mail@gmail.com>
2026-07-29 09:33:57 +0900

    微修正

--- src/main/java/com/example/todolist/controller/MailController.java
+++ src/main/java/com/example/todolist/controller/MailController.java
@@ -120,7 +120,7 @@ public class MailController {
 	private void sendPasswordResetEmail(String email, String token) {
 		
 		String subject = "[todolist]パスワードリセットのご案内";
-		String resetLink = "https://localhost:8080/reset-password?token=" + token;
+		String resetLink = "http://localhost:8080/reset-password?token=" + token;
 		String text = "パスワードリセットのリクエストを受け付けました。下記リンクからリセットをしてください。\n"
 				+ resetLink
 				+ "\nメールに心当たりがない場合、このメールを削除してください。\n"
